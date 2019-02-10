7a03abcb838eca0dc233867bfef805c878d64ecd
Vu Nguyen <vunguyenlethanh@gmail.com>
2019-02-10 11:33:18 +1100

    finalizing the project and adding static folder

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,17 @@ class Blog(models.Model):
 	body = models.TextField()
 	image = models.ImageField(upload_to="images/")
 	
+	
+	def __str__(self):
+		return self.title
+	
+	
+	def summary(self):
+		if len(self.body) < 100:
+			return self.body
+		else:
+			return self.body[:100] + '...'
+	
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
