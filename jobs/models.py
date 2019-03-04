db983fbcdd6fa5a50d6f7d747cf29d0aa9e5597c
Vu Nguyen <vunguyenlethanh@gmail.com>
2019-03-04 18:56:59 +1100

    adding total visitors visual on page

--- jobs/models.py
+++ jobs/models.py
@@ -8,3 +8,22 @@ class Job(models.Model):
 	
 	def __str__(self):
 		return self.summary
+
+
+class UrlHit(models.Model):
+    url     = models.URLField()
+    hits    = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return str(self.url)
+
+    def increase(self):
+        self.hits += 1
+        self.save()
+
+
+class HitCount(models.Model):
+    url_hit = models.ForeignKey(UrlHit, editable=False, on_delete=models.CASCADE)
+    ip      = models.CharField(max_length=40)
+    session = models.CharField(max_length=40)
+    date    = models.DateTimeField(auto_now=True)
